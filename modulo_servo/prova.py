454ad84e5cae62116d10bec2f7c7eaa6431d4218
roccolr <roccolorusso99@live.it>
2024-06-21 15:48:26 +0200

    Aggiunto tutti i files utili

---
+++ modulo_servo/prova.py
@@ -0,0 +1,32 @@
+import socket 
+import sys 
+
+
+servo_ip = '192.168.188.3'
+servo_port = 5050
+forzamento_servo = sys.argv[1]
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_client:
+            # Connessione al server specificato
+            sock_client.connect((servo_ip, servo_port))
+            print("Connessione effettuata con successo. Invio forzamento...")
+            if forzamento_servo == 'plastica':
+                number = 0
+                message = number.to_bytes(4, 'big')
+                # Invio del messaggio al server
+                sock_client.sendall(message)
+            elif forzamento_servo == 'carta':
+                number = 1
+                message = number.to_bytes(4, 'big')
+                # Invio del messaggio al server
+                sock_client.sendall(message)
+                
+            elif forzamento_servo == 'vuoto':
+                number = 2
+                message = number.to_bytes(4, 'big')
+                # Invio del messaggio al server
+                sock_client.sendall(message)
+                
+            else :
+                print("Errore")
+                
