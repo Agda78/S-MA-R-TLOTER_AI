183700a1338427b0261ff5052696f37717743656
Agostino D'Amora <ag.damora@studenti.unina.it>
2024-06-27 11:29:43 +0200

    echo: Aggiunto file .drawio, contenente i vari diagrammi logici

--- ProgettoFinale/avvioProcessoB.m
+++ ProgettoFinale/avvioProcessoB.m
@@ -1,10 +1,11 @@
 function avvioProcessoB(path, forzamento)        
-        % Costruisci il comando di sistema
+    % Costruisci il comando di sistema
     cmd = sprintf('python %s %s', path, forzamento);
     
     % Esegui il comando di sistema e cattura l'output
     [status, result] = system(cmd);
     disp(result)
+    
     % Verifica se ci sono stati errori nell'esecuzione del comando
     if status ~= 0
         error('Errore durante l''esecuzione del comando: %s', result);
