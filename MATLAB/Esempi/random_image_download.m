a171942eddb551f9083e606445a80f8e1699531f
Agostino D'Amora <83039799+Agda78@users.noreply.github.com>
2024-06-29 18:55:32 +0200

    Delete MATLAB directory

--- MATLAB/Esempi/random_image_download.m
+++
@@ -1,8 +0,0 @@
-function random_image_download
-    pythonScript = 'C:\Users\HP\Documents\Prova\S-MA-R-TLOTER_AI\Python\ImmaginiGettate\RandomGetting.py';
-    cmd = sprintf('python %s', pythonScript);
-    status = system(cmd);
-    if status ~= 0
-        error('Errore durante l''esecuzione del comando: %s', result);
-    end
-end
